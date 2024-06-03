5d52ac5141f9d5b904baf26cdd4e66b1633f9d3f
GrahamDo <graham@communicate.za.net>
2024-06-03 14:32:21 +0200

    Implement BuildTrends method

--- Trend.cs
+++ Trend.cs
@@ -2,4 +2,7 @@
 
 internal class Trend
 {
+    public string HashTag { get; set; } = string.Empty;
+    public uint Rank { get; set; }
+    public DateTime LastUsed { get; set; } = new();
 }
--- TrendsBuilder.cs
+++ TrendsBuilder.cs
@@ -4,6 +4,32 @@ internal class TrendsBuilder
 {
     public List<Trend> BuildTrends(List<MastodonPost> posts)
     {
-        throw new NotImplementedException();
+        var results = new List<Trend>();
+        var orderedPosts = posts.OrderBy(p => p.CreatedAt).ToList();
+        //Start at the earliest post so LastUsed is always the last used for each hashtag
+        orderedPosts.ForEach(post =>
+        {
+            post.Tags.ForEach(tag =>
+            {
+                var trend = results.FirstOrDefault(t => t.HashTag == tag.Name);
+                if (trend != null)
+                {
+                    trend.LastUsed = post.CreatedAt;
+                    trend.Rank += CalculateRank(post);
+                }
+                else
+                {
+                    results.Add(new Trend
+                    {
+                        HashTag = tag.Name,
+                        LastUsed = post.CreatedAt,
+                        Rank = CalculateRank(post)
+                    });
+                }
+            });
+        });
+        return results.OrderByDescending(t => t.Rank).ThenByDescending(t => t.LastUsed).ToList();
     }
+
+    private uint CalculateRank(MastodonPost post) => 1 + post.Favourites + (2 * post.Boosts) + (2 * post.Favourites);
 }
