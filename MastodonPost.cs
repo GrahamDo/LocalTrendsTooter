eec288c9b9a653cb554ba439a012436c5a3a964c
GrahamDo <graham@communicate.za.net>
2024-06-03 14:11:56 +0200

    Add Boosts, Favourites, and Replies to model

--- MastodonPost.cs
+++ MastodonPost.cs
@@ -8,4 +8,10 @@ internal class MastodonPost
     public DateTime CreatedAt { get; set; }
     public List<MastodonTag> Tags { get; set; } = [];
     public string Id { get; set; } = string.Empty;
+    [JsonProperty("reblogs_count")]
+    public uint Boosts { get; set; }
+    [JsonProperty("favourites_count")]
+    public uint Favourites { get; set; }
+    [JsonProperty("replies_count")]
+    public uint Replies { get; set; }
 }
