4058a0f61d13fe313b7a812eef70ceadaf3851de
GrahamDo <graham@communicate.za.net>
2024-06-04 13:48:41 +0200

    Implement PostDirect and Post methods

--- MastodonPoster.cs
+++ MastodonPoster.cs
@@ -1,4 +1,8 @@
 ﻿using Newtonsoft.Json;
+using System.Net;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using RestSharp.Authenticators.OAuth2;
 
 namespace LocalTrendsTooter;
 
@@ -9,21 +13,55 @@ internal class MastodonPoster(MaxCharactersCacheManager maxCharactersCacheManage
     public async Task PostPublic(string instanceUrl, string instanceToken, string message) =>
         await Post(instanceUrl, instanceToken, message, false);
 
-    private async Task Post(string instanceUrl, string instanceToken, string message, bool direct)
+    private async Task Post(string instanceUrl, string instanceToken, string message, bool direct, bool isRetry = false)
     {
-        var client = new HttpClient();
-        var characterLimit = await GetTootCharacterLimit(client, instanceUrl);
+        var restOptions = new RestClientOptions($"{instanceUrl}/api/v1/")
+        {
+            Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(instanceToken, "Bearer")
+        };
+        var client = new RestClient(restOptions);
+        
+        var characterLimit = await GetTootCharacterLimit(client);
+        var status = new MastodonStatus { Status = ShortenText(message, characterLimit) };
+        if (direct)
+            status.Visibility = "direct";
+
+        var request = new RestRequest("statuses", Method.Post).AddJsonBody(status);
+        try
+        {
+            await client.PostAsync(request);
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.Message.Contains("Forbidden") || ex.Message.Contains("Unauthorized"))
+                throw new ApplicationException("Invalid Mastodon token");
+            if (ex.StatusCode == HttpStatusCode.UnprocessableEntity && !isRetry)
+            {
+                // It could be the character limit that's decreased. Clear the cache and try again once
+                maxCharactersCacheManager.ClearCache();
+                await Post(instanceUrl, instanceToken, message, direct, true);
+                return;
+            }
+
+            throw;
+        }
     }
 
-    private async Task<int> GetTootCharacterLimit(HttpClient client, string instanceUrl)
+    private static string ShortenText(string message, int characterLimit)
+        => message.Length <= characterLimit ? message : message.Substring(0, characterLimit);
+
+    private async Task<int> GetTootCharacterLimit(RestClient client)
     {
         var charLimit = maxCharactersCacheManager.GetMaxCharacters();
         if (charLimit > 0)
             return charLimit;
 
-        // This assumes the client has been initialised by the Post method
-        var response = await client.GetStringAsync($"{instanceUrl}/api/v1/instance");
-        var results = JsonConvert.DeserializeObject<InstanceInfo>(response) ??
+        var request = new RestRequest("instance");
+        var response = await client.GetAsync(request);
+        if (response?.Content == null)
+            throw new ApplicationException("Empty response from getting instance info");
+        
+        var results = JsonConvert.DeserializeObject<InstanceInfo>(response.Content) ??
             throw new ApplicationException("Can't deserialise instance info");
 
         charLimit = results.Configuration.Statuses.MaxChars;
---
+++ MastodonStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace LocalTrendsTooter;
+
+internal class MastodonStatus
+{
+    public string Status { get; set; } = string.Empty;
+    public string Visibility { get; set; } = "public";
+}
