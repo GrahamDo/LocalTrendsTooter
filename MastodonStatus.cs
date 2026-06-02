4af7333721b475601c211cf97cee84b9a5b5f540
GrahamDo <downs_graham@yahoo.com>
2026-06-02 14:38:00 +0200

    Add JsonProperty attributes to MastodonStatus properties
    This never used to be required by RestSharp, but is now required
    by HttpClient.

--- MastodonStatus.cs
+++ MastodonStatus.cs
@@ -1,7 +1,11 @@
-﻿namespace LocalTrendsTooter;
+﻿using Newtonsoft.Json;
+
+namespace LocalTrendsTooter;
 
 internal class MastodonStatus
 {
+    [JsonProperty("status")]
     public string Status { get; set; } = string.Empty;
+    [JsonProperty("visibility")]
     public string Visibility { get; set; } = "public";
 }
